a2da7eea163266c8fe32abd54b47882b39892a13
sythex1 <tomkendall02@sky.com>
2026-06-11 03:58:36 +0100

    added server status webpage
    server status webpage has been added, ran by a http server seperate to the tcp one used for chatting. minor changes have been made such as attaching the tcp server port to a final variable.

--- chat-project/src/main/java/com/servsoft/Server.java
+++ chat-project/src/main/java/com/servsoft/Server.java
@@ -1,13 +1,19 @@
 package com.servsoft;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.io.PrintWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.io.OutputStream;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
@@ -19,17 +25,23 @@ import java.time.format.DateTimeFormatter;
 
 public class Server implements Runnable{
 
-    private ArrayList<ConnectionHandler> connections;
+    private static final int chatPort = 2211;
+    private static ArrayList<ConnectionHandler> connections;
     private ServerSocket server;
     private ExecutorService pool;
+    private static boolean isOnline = false;
+    public boolean isOnline() {
+        return isOnline;
+    }
+    
 
     public Server() {
         connections = new ArrayList<>();
     }
 
     public static void main(String[] args) {   
-            Server server = new Server();
-            server.run();
+            new Thread(new Server()).start();
+            new Thread(new WebServer(8080)).start();
         }
     
 
@@ -43,8 +55,10 @@ public class Server implements Runnable{
      */
     public void run() {
         try{
-        server = new ServerSocket(2211);
+        server = new ServerSocket(chatPort);
         pool = Executors.newCachedThreadPool();
+        isOnline = true;
+        System.out.println("Chat server started.");
         while (true) {
            Socket client = server.accept();
            ConnectionHandler handler = new ConnectionHandler(client);
@@ -67,7 +81,71 @@ public class Server implements Runnable{
         }
     }
 
+/**
+ * This where the contents of the server status page is handled.
+ * information like if the chat server is online, how many people are connected and what the chat server's port is.
+ */
+    public static class DefaultHandler implements HttpHandler{
 
+        @Override
+        public void handle(HttpExchange exchange) throws IOException{
+            OutputStream out = exchange.getResponseBody();
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append("<html><body>");
+            sb.append("<h1>Chat Server Status</h1>");
+            sb.append("Chat server port is: " + chatPort + "<br>");
+            if(isOnline) {
+            sb.append("Online: True<br>");
+            } else {
+            sb.append("Online: False<br>");
+            }
+            sb.append("Users Online: " + connections.size());
+            sb.append("</body></html>");
+
+            byte[] responseBytes = sb.toString().getBytes("UTF-8");
+            exchange.sendResponseHeaders(200, responseBytes.length);
+            out.write(responseBytes);
+            out.flush();
+            out.close();
+        }
+        
+       
+    }
+
+
+    /**
+     * This section is responsible for establishing the http server, which is used for displaying server status and info on a webpage while its running.
+     */
+    public static class WebServer implements Runnable {
+        private int port;
+
+        WebServer(int port) {
+            this.port = port;
+        }
+
+        public void run() {
+            ExecutorService threadPool = Executors.newCachedThreadPool();
+            Thread.setDefaultUncaughtExceptionHandler((Thread t, Throwable e) -> {
+                e.printStackTrace();
+            });
+            try{
+                HttpServer server = HttpServer.create(new InetSocketAddress("localhost", port), 0);
+                server.createContext("/", new DefaultHandler());
+                server.setExecutor(threadPool);
+                server.start();
+                System.out.println("Http server started.");
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
+        
+    }
+
+    
+    
     /**
      * the user is what every client's information is saved to.
      * 
@@ -88,7 +166,10 @@ public class Server implements Runnable{
         /**
          * This is the main function in the server app, designed to display a message to users when they join, broadcasts to the rest of the user when they join (once they pick a nickname),
          * recieves whatever message they submitted, and sends it to the other users connected. every variable serves an important use here, so I'll go through them.
-         * writer: writes all chat messages sent to a chat log file. this is saved as a .json file by default but can be changed to whatever the operator's preference. the contents are properly saved once the server is closed. if no pre-existing chatlog exists to write to, it will create one itself.
+         * writer: writes all chat messages sent to a chat log file. this is saved as a .json file by default but can be changed to whatever the operator's preference. 
+         * Having a chat log is incredibly important for legal concerns; if someone uses the server for illegal or malicious reasons, its important to have hard proof of them doing so.
+         * having timestamps everytime a message sent is also for this same reason. 
+         * the contents are properly saved once the server is closed. if no pre-existing chatlog exists to write to, it will create one itself.
          * message: stores the message typed out and submitted by the user.
          * dateTime: grabs the current date and time.
          * formatter: formats the date and time into a set pattern. in this case its "dd-mm-yyyy" for the day, and "HH:mm:ss" for the time.
@@ -122,6 +203,9 @@ public class Server implements Runnable{
             } 
 
         }
+
+
+
 
         public void sendMessage(String message) {
             out.println(message);
